f40f66025d663720b6845fd3e230be66c84f920c
Kagan Cin <kcin@thinksmobility.com>
2022-01-24 10:53:41 +0300

    UI improvements have been made

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
-import { useState } from 'react';
-import helper from './helpers/helper';
+import "./App.css";
+import { useState } from "react";
+import helper from "./helpers/helper";
 
 function App() {
   const [xpath, setXPath] = useState(null);
@@ -12,7 +12,12 @@ function App() {
     } catch (error) {
       alert("Input could not be converted");
     }
-  }
+  };
+
+  const copy = () => {
+    navigator.clipboard.writeText(result);
+  };
+
   return (
     <div className="app">
       <h2>xPath To Css Selector</h2>
@@ -21,7 +26,16 @@ function App() {
         <button onClick={convert}>Convert</button>
       </div>
       <div className="result">
-        {result!==null && <p>{result}</p>}
+        {result !== null && (
+          <>
+            <textarea onChange={(e) => setResult(e.target.value)}>
+              {result}
+            </textarea>
+            <button className="btnCopy" onClick={copy}>
+              Copy
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
